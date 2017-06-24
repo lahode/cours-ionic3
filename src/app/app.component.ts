80267ae07e35ceebae382f001275aecdd3bcc18e
Astro <lapi@MacBook-Pro-de-Astro.local>
2017-06-24 02:23:30 +0200

    Add authentification with JWT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,35 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AuthProvider } from "../providers/auth";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'LoginPage';
+  rootPage:string = '';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private platform: Platform,
+              private statusBar: StatusBar,
+              private splashScreen: SplashScreen,
+              private auth: AuthProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.auth.user$.subscribe(user => {
+      if (user) {
+        this.rootPage = 'TabsPage';
+      }
+      else {
+        this.rootPage = 'LoginPage';
+      }
+    });
+
+    this.auth.checkLogin();
   }
+
 }
